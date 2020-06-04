b6ba49a920ce9d3158e99f5ffb90de140a07087e
Fezz <43589668+FBFezz@users.noreply.github.com>
2020-06-04 19:41:07 +0100

    Update and rename __resource.lua to fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+-- Resource Metadata
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'Kiminaze'
+description 'DeathCam'
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
